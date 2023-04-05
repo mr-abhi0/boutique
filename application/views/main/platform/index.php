2870f025eb96590bf1ec59718420ee804269077d
mr-abhi0 <durgapr09138@gmail.com>
2023-04-05 11:47:25 +0530

    first commit

---
+++ application/views/main/platform/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en" class="h-100">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Mr Abhi - Full Stack Web Developer">
+    <meta name="author" content="Mr Abhi">
+    <title>Mr Abhi - Powerful Ecommerce Platform</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <style>
+        nav.nav a {
+            color: white;
+        }
+    </style>
+</head>
+
+<body class="d-flex h-100 text-center text-white bg-dark">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <header class="mb-auto">
+            <div>
+                <h3 class="float-md-start mb-0">Mr Abhi</h3>
+                <nav class="nav nav-masthead justify-content-center float-md-end">
+                    About Platform</a>
+                </nav>
+            </div>
+        </header>
+
+        <main class="px-3">
+            <h1>Ecommerce-CodeIgniter-Bootstrap</h1>
+            <p class="lead">Find out big database of Open Source projects.</p>
+            <p class="lead">
+            </p>
+        </main>
+
+        <footer class="mt-auto text-white-50">
+            <p>Cover template for Boutiqe fashion</p>
+        </footer>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
